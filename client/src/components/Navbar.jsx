f4c6ae7999ffa65841cb917e8c6f8f03b3f4d797
sharvin18 <dedhiasharvin@gmail.com>
2022-02-18 13:05:48 +0530

    frontend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,7 +19,8 @@ const Navbar = () => {
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
-                <img src={logo} alt="logo" className='w-32 cursor-pointer'/>
+                {/* <img src={logo} alt="logo" className='w-32 cursor-pointer'/> */}
+                <h1 className='text-white flex text-5xl font-serif antialiased'>CRYPTON</h1>
             </div>
             <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
                 {["Market", "Exchange", "Tutorial", "Wallets"].map((item, index) => (
